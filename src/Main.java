ba0e5ec0535d6debdcfe3c730862468fc88ff922
Ksenia <ksuskaxD@gmail.com>
2022-06-20 22:47:49 +0300

    1 миля за 20 руб

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 10_000;
+        int milePrice = 20;
+        int milesEarned = ticketPrice / milePrice;
+        System.out.println("Начислено баллов: " + milesEarned);
+
+    }
+}
